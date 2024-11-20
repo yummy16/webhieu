7a0138aed5783cdff663187884997dd4a7f64e3a
yummy16 <hieunhungng@gmail.com>
2024-11-20 22:46:15 +0100

    ajout

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-document.getElementById('next').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+
+  // Initialise Swiper avec les options de base
+const swiper = new Swiper('.mySwiper', {
+    // Activer la pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Activer la navigation (flèches)
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // Activer le défilement infini
+    loop: true,
+  });
   
